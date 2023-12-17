2b58a56a5fd2b4715c2f249998c6e01d6db24b2b
Selteq <anas@selteq.net>
2023-12-17 23:13:11 +0500

    sdsd

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
 import '@/styles/globals.css'
+import i18n from './i18n'
+import { I18nextProvider } from 'react-i18next'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+    <I18nextProvider i18n={i18n}>
+      <Component {...pageProps} />
+    </I18nextProvider>
+  )
+
 }
---
+++ src/pages/i18n.js
@@ -0,0 +1,31 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import enTranslation from '../../public/locales/en/translation.json';
+import arTranslation from '../../public/locales/ar/translation.json';
+import frTranslation from '../../public/locales/fr/translation.json';
+import itTranslation from '../../public/locales/it/translation.json';
+import ruTranslation from '../../public/locales/ru/translation.json';
+import chTranslation from '../../public/locales/ch/translation.json';
+
+const resources = {
+  en: { translation: enTranslation },
+  ar: { translation: arTranslation },
+  fr: { translation: frTranslation },
+  it: { translation: itTranslation },
+  ru: { translation: ruTranslation },
+  ch: { translation: chTranslation },
+};
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: 'en', // Default language
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
